03cf1eddf5a10a889a7082c319d484aa789dd0a6
River Stahley <rstahley8@gmail.com>
2025-05-10 12:55:27 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,8 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Create a new instance of the GameGUI class to display the GUI
         GameGUI gameGUI = new GameGUI();
-        gameGUI.startScreen();
-
-        
+        gameGUI.startScreen();    
     }
 }
